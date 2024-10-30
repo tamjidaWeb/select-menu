dabe078566879291612699bc56e12b66f64d3535
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-30 14:29:40 +0600

    select menu commited

---
+++ script.js
@@ -0,0 +1,20 @@
+var selectField = document.getElementById('select-field');
+var selectText = document.getElementById('select-text');
+var options = document.getElementsByClassName('options');
+var list = document.getElementById('lists');
+var arrowIcon = document.getElementById('arrow-icon');
+
+
+selectField.onclick = function(){
+    list.classList.toggle('hide');
+    arrowIcon.classList.toggle('rotate');
+
+}
+
+for(option of options){
+    option.onclick = function(){
+        selectText.innerHTML = this.textContent;
+        list.classList.toggle('hide');
+        arrowIcon.classList.toggle('rotate');
+    }
+}
